b25cc6e34a8c41d14f6ff90ee6c35c474334e1a8
Erasin <erasinoo@gmail.com>
2025-06-02 12:21:25 +0800

    helix statusline

--- waybar/scripts/lyrics.sh
+++ waybar/scripts/lyrics.sh
@@ -3,7 +3,7 @@
 id=$(niri msg --json windows | jq -r '.[] | select(.title=="Lyrics").id')
 
 if [ -z $id ];then
-  niri msg action spawn -- alacritty -T "Lyrics" -e lyrics-next;
+  niri msg action spawn -- alacritty -o window.dimensions.columns=40 -o window.dimensions.lines=40 -T "Lyrics" -e lyrics-next;
 else
   niri msg action close-window --id $id;
 fi
