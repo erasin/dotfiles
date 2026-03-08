bb6543637db33a7c26da2c35a5783800f411c5e5
Erasin <erasinoo@gmail.com>
2026-03-08 15:30:10 +0800

    mpd

--- waybar/scripts/rmpc-picker.sh
+++ waybar/scripts/rmpc-picker.sh
@@ -4,7 +4,8 @@ id=$(niri msg --json windows | jaq -r '.[] | select(.title=="Rmpc").id')
 
 if [ -z $id ];then
   # alacritty 与 niri 窗口大小处理协议问题？
-  niri msg action spawn -- alacritty -T "Rmpc" -e rmpc;
+  niri msg action spawn -- euphonica;
+  # niri msg action spawn -- alacritty -T "Rmpc" -e rmpc;
   # niri msg action spawn -- kitty -o confirm_os_window_close=0 -T "Rmpc" -e rmpc;
 else
   niri msg action close-window --id $id;
