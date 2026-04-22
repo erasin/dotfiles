94c90360f9eb023c1f79ccd2f86db314cfad65cb
Erasin <erasinoo@gmail.com>
2026-04-22 13:58:53 +0800

    wsl

--- windows/wsl.sh
+++ windows/wsl.sh
@@ -12,18 +12,7 @@ export DISPLAY=:0
 # 如果使用 Wayland
 export WAYLAND_DISPLAY=wayland-0
 
-
-# wsl 中启用 ssh-agent
-if ! pgrep -x ssh-agent > /dev/null; then
-    eval "$(ssh-agent -s)"
-fi
-
-if ! ssh-add -l &>/dev/null; then
-    [[ -f ~/.ssh/id_ed25519 ]] && ssh-add ~/.ssh/id_ed25519
-    [[ -f ~/.ssh/id_rsa ]] && ssh-add ~/.ssh/id_rsa
-fi
-
-# 命令不可用
+# # 命令不可用
 # if ! command -v cmd.exe &> /dev/null; then
 #     # sudo sh -c 'echo :WSLInterop:M::MZ::/init:PF > /usr/lib/binfmt.d/WSLInterop.conf'
 #     sudo systemctl unmask systemd-binfmt.service
